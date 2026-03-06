915b89d1b61662ba4fdeb672ed9ad4ec0269c3c5
Anamika Patel <83943502+Anamika024@users.noreply.github.com>
2026-03-06 21:03:05 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1784-check-if-binary-string-has-at-most-one-segment-of-ones/1784-check-if-binary-string-has-at-most-one-segment-of-ones.java
+++ 1784-check-if-binary-string-has-at-most-one-segment-of-ones/1784-check-if-binary-string-has-at-most-one-segment-of-ones.java
@@ -1,17 +1,19 @@
 class Solution {
     public boolean checkOnesSegment(String s) {
-        int count=0, sum=0;
-        for(char ch : s.toCharArray()) {
-            if(ch=='1') sum++;
+        if(s.contains("01")) return false;
+        return true;
+        // int count=0, sum=0;
+        // for(char ch : s.toCharArray()) {
+        //     if(ch=='1') sum++;
 
-        }
-        for(char ch : s.toCharArray()) {
-            if(ch=='1') {
-                count++;
-            }else {
-                break;
-            }
-        }
-        return count==sum;
+        // }
+        // for(char ch : s.toCharArray()) {
+        //     if(ch=='1') {
+        //         count++;
+        //     }else {
+        //         break;
+        //     }
+        // }
+        // return count==sum;
     }
 }
