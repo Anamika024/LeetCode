2a58a9882e643e6c08ab0700aca21ec19487654e
Anamika Patel <83943502+Anamika024@users.noreply.github.com>
2026-03-14 19:21:36 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1415-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n/1415-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n.java
+++ 1415-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n/1415-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n.java
@@ -1,23 +1,29 @@
 class Solution {
-    PriorityQueue<String> pq = new PriorityQueue<>();
+    // PriorityQueue<String> pq = new PriorityQueue<>();
+    int count=0;
+    String ans="";
+    char[] chars = {'a','b','c'};
     public String getHappyString(int n, int k) {
         char[] chars = {'a','b','c'};
-        solve(n, new StringBuilder(), chars);
-        if(k>pq.size()) return "";
-        while(k-->1) {
-            pq.poll();
-        }
-        return pq.poll();
+        solve(n, new StringBuilder(), k);
+        // if(k>pq.size()) return "";
+        // while(k-->1) {
+        //     pq.poll();
+        // }
+        // return pq.poll();
+        return ans;
     }
-    public void solve(int n, StringBuilder sb, char[] chars) {
+    public void solve(int n, StringBuilder sb, int k) {
         if(sb.length()==n) {
-            pq.add(sb.toString());
+            // pq.add(sb.toString());
+            count++;
+            if(count==k) ans=sb.toString();
             return;
         }
         for(char c : chars) {
             if(sb.length()>0 && sb.charAt(sb.length()-1)==c) continue;
             sb.append(c);
-            solve(n, sb, chars);
+            solve(n, sb, k);
             sb.deleteCharAt(sb.length()-1);
         }
     }
