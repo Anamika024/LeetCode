886290a1a70f945c0db03de3b423b42e49b64214
Anamika Patel <83943502+Anamika024@users.noreply.github.com>
2026-03-16 13:21:56 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1878-get-biggest-three-rhombus-sums-in-a-grid/1878-get-biggest-three-rhombus-sums-in-a-grid.java
+++ 1878-get-biggest-three-rhombus-sums-in-a-grid/1878-get-biggest-three-rhombus-sums-in-a-grid.java
@@ -4,18 +4,31 @@ class Solution {
 
         int m = grid.length;
         int n = grid[0].length;
-
+        int d1[][] = new int[m][n];//top left to bottom right diagonal
         for(int r=0; r<m; r++) {
             for(int c=0; c<n; c++) {
-                set.add(grid[r][c]); //for side=0
-                for(int s=1; r-s>=0 && r+s<m && c-s>=0 && c+s<n; s++) {
-                    int sum=0;
-                    for(int k=0; k<s; k++) {
-                        sum+=grid[r-s+k][c+k];//top to right diagonal
-                        sum+=grid[r+k][c+s-k];//right to bottom diagonal
-                        sum+=grid[r+s-k][c-k];//bottom to left diagonal
-                        sum+=grid[r-k][c-s+k];//left to top diagonal
-                    }
+                d1[r][c]=grid[r][c];
+                if(r>0 && c>0) d1[r][c]+=d1[r-1][c-1];
+            }
+        }
+        int d2[][] = new int[m][n];//top right to bottom left diagonal
+        for(int r=0; r<m; r++) {
+            for(int c=n-1; c>=0; c--) {
+                d2[r][c]=grid[r][c];
+                if(r>0 && c<n-1) d2[r][c]+=d2[r-1][c+1];
+            }
+        }
+        for(int r=0;r<m;r++){
+            for(int c=0;c<n;c++){
+                set.add(grid[r][c]);
+                for(int s=1; r-s>=0 && r+s<m && c-s>=0 && c+s<n; s++){
+                    int sum = 0;
+                    sum += d1[r][c+s]-(r-s-1>=0 && c-1>=0? d1[r-s-1][c-1] : 0);
+                    sum += d2[r+s][c]-(r-1>=0 && c+s+1<n ? d2[r-1][c+s+1] : 0);
+                    sum += d1[r+s][c]-(r-1>=0 && c-s-1>=0 ? d1[r-1][c-s-1] : 0);
+                    sum += d2[r][c-s]-(r-s-1>=0 && c+1<n ? d2[r-s-1][c+1] : 0);
+                    sum -= (grid[r-s][c]+grid[r][c+s]+grid[r+s][c]+grid[r][c-s]);
+
                     set.add(sum);
                 }
             }
