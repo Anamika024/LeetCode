6a9a9388b73ac79c36e6a8c2960fb5d526125dc1
Anamika Patel <83943502+Anamika024@users.noreply.github.com>
2026-03-25 10:26:20 +0530

    Time: 14 ms (14.63%), Space: 102 MB (22.44%) - LeetHub

--- 2906-construct-product-matrix/2906-construct-product-matrix.java
+++ 2906-construct-product-matrix/2906-construct-product-matrix.java
@@ -3,15 +3,18 @@ class Solution {
     public int[][] constructProductMatrix(int[][] grid) {
         int n = grid.length;
         int m = grid[0].length;
+
         int prefix[][] = new int[n][m];
         int suffix[][] = new int[n][m];
-        int p=1, s=1;
+
+        int p=1;
         for(int i=0; i<n; i++) {
             for(int j=0; j<m; j++) {
                 prefix[i][j]=p;
                 p=(p*(grid[i][j]%mod))%mod;
             }
         }
+        int s=1;
         for(int i=n-1; i>=0; i--) {
             for(int j=m-1; j>=0; j--) {
                 suffix[i][j]=s;
@@ -20,11 +23,11 @@ class Solution {
         }
         for(int i=0; i<n; i++) {
             for(int j=0; j<m; j++) {
-                System.out.print(prefix[i][j] + " ");
                 grid[i][j]=(prefix[i][j]*suffix[i][j])%mod;
             }
-            System.out.println();
         }
         return grid;
+
+        
     }
 }
