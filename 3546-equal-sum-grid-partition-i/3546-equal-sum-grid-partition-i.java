73b14ebc35690f66690d11b3507f4aceef92328f
Anamika Patel <83943502+Anamika024@users.noreply.github.com>
2026-03-25 23:34:43 +0530

    Time: 7 ms (21.68%), Space: 156.5 MB (100%) - LeetHub

--- 3546-equal-sum-grid-partition-i/3546-equal-sum-grid-partition-i.java
+++ 3546-equal-sum-grid-partition-i/3546-equal-sum-grid-partition-i.java
@@ -1,31 +1,50 @@
 class Solution {
     public boolean canPartitionGrid(int[][] grid) {
-        int n = grid.length;
-        int m = grid[0].length;
-
-        long total=0;
-        for(int r[] : grid) {
-            for(int v : r) {
-                total+=v;
+        int m = grid.length;
+        int n = grid[0].length;
+        long rowSum1[] = new long[m];
+        for(int i=0; i<m; i++) {
+            long sum=0;
+            for(int j=0; j<n; j++) {
+                sum+=grid[i][j];
             }
+            rowSum1[i]= i==0 ? sum : rowSum1[i-1]+sum;
+        }
+        long rowSum2[] = new long[m];
+        for(int i=m-1; i>=0; i--) {
+            long sum=0;
+            for(int j=0; j<n; j++) {
+                sum+=grid[i][j];
+            }
+            rowSum2[i]= i==m-1 ? sum : rowSum2[i+1]+sum;
         }
 
-        long curr=0;
-        for(int i=0; i<n; i++) {
-            for(int j=0; j<m; j++) {
-                curr+=grid[i][j];
+        for(int i=0; i<m-1; i++) {
+            if(rowSum1[i]==rowSum2[i+1]) {
+                return true;
             }
-            if(curr==total-curr) return true;
         }
-        curr=0;
-        for(int j=0; j<m; j++) {
-            for(int i=0; i<n; i++) {
-                curr+=grid[i][j];
+        long colSum1[] = new long[n];
+        for(int j=0; j<n; j++) {
+            long sum=0;
+            for(int i=0; i<m; i++) {
+                sum+=grid[i][j];
             }
-            if(curr==total-curr) return true;
+            colSum1[j]= j==0 ? sum : colSum1[j-1]+sum;
+        }
+        long colSum2[] = new long[n];
+        for(int j=n-1; j>=0; j--) {
+            long sum=0;
+            for(int i=0; i<m; i++) {
+                sum+=grid[i][j];
+            }
+            colSum2[j]= j==n-1 ? sum : colSum2[j+1]+sum;
+        }
+        for(int j=0; j<n-1; j++) {
+            if(colSum1[j]==colSum2[j+1]) return true;
         }
         return false;
-
+        
 
         
     }
