9429497290598fabb3f4710b49da6a2e516ce536
Anamika Patel <83943502+Anamika024@users.noreply.github.com>
2026-04-12 17:31:02 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1320-minimum-distance-to-type-a-word-using-two-fingers/1320-minimum-distance-to-type-a-word-using-two-fingers.java
+++ 1320-minimum-distance-to-type-a-word-using-two-fingers/1320-minimum-distance-to-type-a-word-using-two-fingers.java
@@ -1,33 +1,37 @@
 class Solution {
-    Integer dp[][][][][]= new Integer[301][7][7][7][7];
+    Integer dp[][][] = new Integer[301][27][27];
     public int minimumDistance(String word) {
-        return solve(word, 0, -1, -1, -1, -1);
+        return solve(word, 0, 26, 26);
     }
-    public int solve(String word, int i, int x1, int y1, int x2, int y2) {
+    public int solve(String word, int i, int f1, int f2) {
         if(i>=word.length()) {
             return 0;
         }
-        if(dp[i][x1+1][y1+1][x2+1][y2+1]!=null) {
-            return dp[i][x1+1][y1+1][x2+1][y2+1];
+        if(dp[i][f1][f2]!=null) {
+            return dp[i][f1][f2];
         }
-        int r = (word.charAt(i)-'A')/6;
-        int c = (word.charAt(i)-'A')%6;
+        int curr = word.charAt(i)-'A';
 
         //non fingers used
-        if(x1==-1 && y1==-1 && x2==-1 && y2==-1) {
-            return dp[i][x1+1][y1+1][x2+1][y2+1]=solve(word, i+1, r, c, x2, y2);
+        if(f1==26 && f2==26) {
+            return dp[i][f1][f2]=solve(word, i+1, curr, f2);
         }
         //f2 not used
-        if(x2==-1 && y2==-1) {
-            int moveF2=solve(word, i+1, x1, y1, r, c);
-            int moveF1=Math.abs(x1-r)+Math.abs(y1-c)+solve(word, i+1, r, c, x2, y2);
-            return dp[i][x1+1][y1+1][x2+1][y2+1]=Math.min(moveF1, moveF2);
+        if(f2==26) {
+            int moveF2=solve(word, i+1, f1, curr);
+            int moveF1=getDist(f1, curr)+solve(word, i+1, curr, f2);
+            return dp[i][f1][f2]=Math.min(moveF1, moveF2);
         }
         //both used
-        int moveF2=Math.abs(x2-r)+Math.abs(y2-c)+solve(word, i+1, x1, y1, r, c);
-        int moveF1=Math.abs(x1-r)+Math.abs(y1-c)+solve(word, i+1, r, c, x2, y2);
+        int moveF2=getDist(f2, curr)+solve(word, i+1, f1, curr);
+        int moveF1=getDist(f1, curr)+solve(word, i+1, curr, f2);
 
-        return dp[i][x1+1][y1+1][x2+1][y2+1]=Math.min(moveF1, moveF2);
+        return dp[i][f1][f2]=Math.min(moveF1, moveF2);
 
+    }
+    public int getDist(int pos1, int pos2) {
+        int r1 = pos1/6, c1=pos1%6;
+        int r2 = pos2/6, c2=pos2%6;
+        return Math.abs(r1-r2)+Math.abs(c1-c2);
     }
 }
