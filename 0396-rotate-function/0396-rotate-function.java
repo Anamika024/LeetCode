b9e0d1a6403057b90a37bd5a1e8cea9ec406deae
Anamika Patel <83943502+Anamika024@users.noreply.github.com>
2026-05-02 00:39:02 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0396-rotate-function/0396-rotate-function.java
+++ 0396-rotate-function/0396-rotate-function.java
@@ -3,18 +3,18 @@ class Solution {
         int n=nums.length;
         //f(1)=f(0)+sum-n*nums[n-1]
         //f(k)=f(k-1)+sum-n*nums[n-k];
-        
-        long f=0, sum=0;
+
+        int f=0, sum=0;
         for(int i=0; i<n; i++) {
             sum+=nums[i];
             f+=nums[i]*i;
         }
-        long max=f;
+        int max=f;
         for(int k=1; k<n; k++) {
-            f=f+sum-(long)n*nums[n-k];
+            f=f+sum-n*nums[n-k];
             max=Math.max(max, f);
         }
-        return max==Long.MIN_VALUE ? 0 : (int)max;
+        return max;
         
     }
 }
