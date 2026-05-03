e19a7b7a2cd3263d75282642730933bcf93943f7
Anamika Patel <83943502+Anamika024@users.noreply.github.com>
2026-05-03 23:48:37 +0530

    Time: 1 ms (23.75%), Space: 43.1 MB (14.29%) - LeetHub

---
+++ 0796-rotate-string/0796-rotate-string.java
@@ -0,0 +1,22 @@
+class Solution {
+    public boolean rotateString(String s, String goal) {
+        int n = s.length();
+        char[] ch = s.toCharArray();
+        while(n-->0) {
+            rotate(ch);
+            // System.out.print(new String(ch) + " ");
+            if(new String(ch).equals(goal)) return true;
+        }
+        return false;
+        
+    }
+    public void rotate(char[] arr) {
+        int n = arr.length;
+        char prev=arr[0];
+        for(int i=n-1; i>=0; i--) {
+            char curr=arr[i];
+            arr[i]=prev;
+            prev=curr;
+        }
+    }
+}
