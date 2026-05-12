ca9c328bf08e895d1d698c15e7420f7c2c1976ed
Anamika Patel <83943502+Anamika024@users.noreply.github.com>
2026-05-12 22:16:50 +0530

    Time: 1503 ms (5.77%), Space: 114 MB (29.81%) - LeetHub

--- 1665-minimum-initial-energy-to-finish-tasks/1665-minimum-initial-energy-to-finish-tasks.java
+++ 1665-minimum-initial-energy-to-finish-tasks/1665-minimum-initial-energy-to-finish-tasks.java
@@ -1,21 +1,22 @@
 class Solution {
     public int minimumEffort(int[][] tasks) {
         int n = tasks.length;
-        int[][] mat = new int[n][3];
-        for(int i=0; i<n; i++) {
-            mat[i][0]=tasks[i][0];
-            mat[i][1]=tasks[i][1];
-            mat[i][2]=Math.abs(tasks[i][1]-tasks[i][0]);
-        }
-        Arrays.sort(mat, (a, b) -> (b[2]-a[2]));
+        // int[][] mat = new int[n][3];
+        // for(int i=0; i<n; i++) {
+        //     mat[i][0]=tasks[i][0];
+        //     mat[i][1]=tasks[i][1];
+        //     mat[i][2]=Math.abs(tasks[i][1]-tasks[i][0]);
+        // }
+        // Arrays.sort(mat, (a, b) -> (b[2]-a[2]));
 
+        Arrays.sort(tasks, (a, b) -> (b[1]-b[0])-(a[1]-a[0])); // can replace separate mat[][] and it will reduce space complexity
         int sum1=0, sum2=0;
         for(int task[] : tasks) {
             sum1+=task[0];
             sum2+=task[1];
         }
         for(int e=sum1; e<=sum2; e++) {
-            if(isValid(e, mat)) {
+            if(isValid(e, tasks)) {
                 return e;
             }
         } 
