c51ca0c0951bbbcfc20a82ed4513346df9ffe9f9
Anamika Patel <83943502+Anamika024@users.noreply.github.com>
2026-05-17 11:54:46 +0530

    Time: 1 ms (99.73%), Space: 52.2 MB (46.11%) - LeetHub

--- 1306-jump-game-iii/1306-jump-game-iii.java
+++ 1306-jump-game-iii/1306-jump-game-iii.java
@@ -1,20 +1,16 @@
 class Solution {
     public boolean canReach(int[] arr, int start) {
-        return canReach(arr, start, new boolean[arr.length]);
-
-    }
-    public boolean canReach(int[] arr, int i, boolean[] visited) {
-        if(i<0 || i>=arr.length || visited[i]) {
+        if(start<0 || start>=arr.length || arr[start]<0) {
             return false;
         }
-        if(arr[i]==0) {
+        if(arr[start]==0) {
             return true;
         }
-        visited[i]=true;
-        
-        boolean right = canReach(arr, i+arr[i], visited);
-        boolean left = canReach(arr, i-arr[i], visited);
-        
-        return left || right;
+        arr[start]=-arr[start];//marked as visited
+
+        boolean right = canReach(arr, start+arr[start]);
+        boolean left = canReach(arr, start-arr[start]);
+
+        return left||right;
     }
 }
