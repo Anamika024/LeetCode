8e397c375666f2e3f361f196d3d45112208d48da
Anamika Patel <83943502+Anamika024@users.noreply.github.com>
2026-05-19 00:08:39 +0530

    Time: 57 ms (52.29%), Space: 76.6 MB (88.47%) - LeetHub

---
+++ 1345-jump-game-iv/1345-jump-game-iv.java
@@ -0,0 +1,46 @@
+class Solution {
+    public int minJumps(int[] arr) {
+        int n = arr.length;
+        HashMap<Integer, List<Integer>> map = new HashMap<>();
+        for(int i=0; i<n; i++) {
+            map.putIfAbsent(arr[i], new ArrayList<>());
+            map.get(arr[i]).add(i);
+        }
+
+        boolean[] visited = new boolean[n];
+        Queue<Integer> q = new LinkedList<>();
+        q.add(0);
+        visited[0]=true;
+        int steps=0;
+        while(!q.isEmpty()) {
+            int size = q.size();
+
+            while(size-->0) {
+                int i = q.poll();
+                if(i==n-1) return steps;
+
+                if(map.containsKey(arr[i])) {
+                    for(int next : map.get(arr[i])) {
+                        if(!visited[next]) {
+                            visited[next]=true;
+                            q.offer(next);
+                        }
+                    }
+                    map.remove(arr[i]);
+                }
+                if(i-1>=0 && !visited[i-1]) {
+                    visited[i-1]=true;
+                    q.offer(i-1);
+                }
+                if(i+1<n && !visited[i+1]) {
+                    visited[i+1]=true;
+                    q.offer(i+1);
+                }
+            }
+            steps++;
+
+        }
+        return -1;
+        
+    }
+}
