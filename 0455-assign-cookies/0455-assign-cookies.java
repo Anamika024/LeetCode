875dbd9f3fbbe16cc3713be82c5859c02ea835b5
Anamika Patel <83943502+Anamika024@users.noreply.github.com>
2026-05-20 17:12:07 +0530

    Time: 16 ms (11.62%), Space: 51.6 MB (35.97%) - LeetHub

--- 0455-assign-cookies/0455-assign-cookies.java
+++ 0455-assign-cookies/0455-assign-cookies.java
@@ -2,12 +2,6 @@ class Solution {
     public int findContentChildren(int[] g, int[] s) {
         Arrays.sort(g);
         Arrays.sort(s);
-
-        // int maxG=g[g.length-1];
-        // int maxS=s[s.length-1];
-
-        // int minLen = Math.min(g.length, s.length);
-
         int i=0, j=0, ans=0;
         while(i<g.length && j<s.length) {
             if(s[j]>=g[i]) {
@@ -18,7 +12,7 @@ class Solution {
                 j++;
             }
         }
-        return ans;
+        return ans; //or return i
 
         
     }
