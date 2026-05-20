3b9c4de39053038899c03f36cc4ed5598fded634
Anamika Patel <83943502+Anamika024@users.noreply.github.com>
2026-05-20 23:41:15 +0530

    Time: 70 ms (46.78%), Space: 103.1 MB (76.48%) - LeetHub

--- 1353-maximum-number-of-events-that-can-be-attended/1353-maximum-number-of-events-that-can-be-attended.java
+++ 1353-maximum-number-of-events-that-can-be-attended/1353-maximum-number-of-events-that-can-be-attended.java
@@ -2,7 +2,7 @@ class Solution {
     public int maxEvents(int[][] events) {
         int n = events.length;
 
-        Arrays.sort(events, Comparator.comparing(a->a[0]));
+        Arrays.sort(events, (a, b)-> a[0]-b[0]);
 
         PriorityQueue<Integer> pq = new PriorityQueue<>();
         int day=events[0][0];
@@ -10,17 +10,18 @@ class Solution {
         int count=0;
 
         while(i<n || !pq.isEmpty()) {
-            while(i<n  && events[i][0]==day) {
+            while(i<n  && events[i][0]==day) { // add all available events at day "day"
                 pq.offer(events[i][1]);
                 i++;
             }
 
-            if(!pq.isEmpty()) {
+            if(!pq.isEmpty()) { // attend event ending soonest
                 pq.poll();
                 count++;
             }
             day++;
-
+            
+            // remove old events whose day is finished
             while(!pq.isEmpty() && pq.peek()<day) {
                 pq.poll();
             }
